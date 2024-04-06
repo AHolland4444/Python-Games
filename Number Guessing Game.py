6de5833e7374219821281be9a42b99541f470331
Andrew Holland <101781895+AHolland4444@users.noreply.github.com>
2024-04-06 17:35:15 -0400

    Updated Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,32 +1,45 @@
+
+
 # Guess the Number Game
-# Input an integer from the range of 1 through 10.
+# Input an integer from the range of 1 through 100.
 
-import random
-end = False
-lives = 10
-comp = random.randint(1,100)
-while (lives >= 1):
-    user = int(input("Your guess: "))
-    lives -= 1
+while True:
+    print("""
+Guess the number!
+It's a number between 1 through 100.
+You have 10 tries.
+Go!
+""")
 
-    if (user > comp):
-        print("Your number is too high. Try again.")
-        print("Remaining tries:", lives)
-    elif (user < comp):
-        print("Your number is too low. Try again.")
-        print("Remaining tries:", lives)
-    else:
+    import random
+    end = False
+    lives = 10
+    comp = random.randint(1,100)
+    while (lives >= 1):
+        user = int(input("Your guess: "))
+        lives -= 1
+
+        if (user > comp):
+            print("Your number is too high. Try again.")
+            print("Remaining tries:", lives)
+        elif (user < comp):
+            print("Your number is too low. Try again.")
+            print("Remaining tries:", lives)
+        else:
+            lives = 0
+            continue
+
+    if (comp == user):
+        print("The computer's number was",comp,".", "You Win!")
         lives = 0
+        print("Congratulations!")
+    else:
+        print("The computer's number was",comp,".", "You Lose!")
+        lives = 0
+        print("Game over.")
+
+    reboot = input("Play again? (Y/N): ")
+    if (reboot == "N") or (reboot == "n"):
+        break
+    else:
         continue
-
-if (comp == user):
-    print("The computer's number was",comp,".", "You Win!")
-    lives = 0
-    end = True
-else:
-    print("The computer's number was",comp,".", "You Lose!")
-    lives = 0
-    end = True
-
-if (end == True):
-    print("The end.")
